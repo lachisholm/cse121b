5b75f9520ab3cbae0f29a302cc5605f41ac64cc6
Lora Chisholm <loraperrychisholm@gmail.com>
2024-01-25 23:17:08 -0700

    fix change

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -8,7 +8,7 @@ function add(num1,num2) {
 }
 
 function addNumbers() {
-    const num1 =parseInt(document.getElementById("add1").value);
+    const num1 = parseInt(document.getElementById("add1").value);
     const num2 = parseInt(documnet.querySelector("#add2").value);
     const sum = add(num1, num2);
 
