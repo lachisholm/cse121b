d40a6ed868effa53b5a8ec450a4edecf5294deb6
Lora Chisholm <loraperrychisholm@gmail.com>
2024-01-25 22:52:11 -0700

    changed

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -3,20 +3,18 @@
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
 
-function add(num1,num2)
-{
+function add(num1,num2) {
     return num1 + num2;
 }
 
-function addNumbers() 
-{
-    const num1 =document.getElementById("add1").value;
-    const num2 = documnet.querySelector("#add2").value;
+function addNumbers() {
+    const num1 =parseInt(document.getElementById("add1").value);
+    const num2 = parseInt(documnet.querySelector("#add2").value);
     const sum = add(num1, num2);
 
     const sumField = document.getElementById("sum");
     sumField.value = sum;
-    
+
 }
 
 document.getElementByID("addNumbers").addEventListener("click",addNumbers)
